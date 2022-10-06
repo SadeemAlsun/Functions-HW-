ad5ce7d17558d0cffc84ce068b846ca6db1bf3f7
SADEEM MOHAMMED <114874826+SadeemAlsun@users.noreply.github.com>
2022-10-06 07:42:50 +0300

    Add files via upload

---
+++ sad.kt
@@ -0,0 +1,18 @@
+package com.example.firsttry
+
+fun main() {
+    Exercise(3.2,6.0,"*")
+    Exercise(3.8,34.0,"/")
+    Exercise(8.1,7.0,"-")
+    Exercise(9.4,2.8)
+}
+fun Exercise(dub1:Double , dub2: Double , str:String="+"){
+        when(str){
+        "-"-> println("the Total of this mathematical operation is :${dub1-dub2}")
+        "*"-> println ("the Total of this mathematical operation is: ${dub1*dub2}")
+        "/"-> if(dub1>dub2)
+            println("the Total of this mathematical operation is :${dub1/dub2}")
+        else println("the Total of this mathematical operation is: ${dub2/dub1}")
+       "+"-> println ("the Total of this mathematical operation is :${dub1+dub2}")
+            else-> println("invalid arithmetic operation ,please try again")
+    }}
